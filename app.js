b24218814f4ed80c8b41ca2d102312fdeede60cd
Edward Binion <eddiebinion2@gmail.com>
2022-04-05 21:03:46 -0500

    parse api data

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const express = require('express');
+const https = require('https');
 const app = express();
 const PORT = 3000;
-const starturl = 'http://api.weatherapi.com/v1/current.json?key=5a7b27039073496996c34110223103&q=';
+// const starturl = 'http://api.weatherapi.com/v1/current.json?key=5a7b27039073496996c34110223103&q=';
 // const place = 
-const endurl = '&aqi=no'
+// const endurl = '&aqi=no'
 
-// const url = starturl + place + endurl;
+const url = 'https://api.weatherapi.com/v1/current.json?key=5a7b27039073496996c34110223103&q=Chicago&aqi=no';
 app.get('/', (req,res)=>{
+    https.get(url, res=>{
+        console.log(res.statusCode);
+
+        res.on('data', data=>{
+            const weatherData = JSON.parse(data);
+            console.log(weatherData);
+        })
+    })
     res.send('App Running!!!')
 })
 
 app.listen(PORT, (req,res)=>{
-    console.log('App 🏃🏽‍♂️🏃🏽‍♂️🏃🏽‍♂️ on PORT ' + PORT + '!!!');
+    console.log('Server 🏃🏽‍♂️🏃🏽‍♂️🏃🏽‍♂️ on PORT ' + PORT + '!!!');
 })
